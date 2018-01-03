8a133894d9685e4522fe64d67f3104c92e014c10
tirea02 <tirea02@gmail.com>
2018-01-03 16:43:58 +0900

    version 0.4

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -99,6 +99,42 @@
 
 })(jQuery);
 
+/* time line viewport js*/
+(function() {
+
+    'use strict';
+
+    // define variables
+    var items = document.querySelectorAll(".timeline li");
+
+    // check if an element is in viewport
+    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function callbackFunc() {
+        for (var i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+                items[i].classList.add("in-view");
+                console.log("trigerd");
+            }
+        }
+    }
+
+    // listen for events
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
+
+})();
+
 
 $('#test_type').typeIt({
     strings: ['summon bot', 'play youtube link', 'make your own playlist'],
@@ -107,3 +143,4 @@ $('#test_type').typeIt({
     breakLines: false,
     autoStart: false
 });
+
